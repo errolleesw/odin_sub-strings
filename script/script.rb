ed353e50db2cc4f4d15dea08332674b133fb5b56
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2023-03-26 11:57:40 +1100

    update for multiple words and remove case sensitivity

--- script/script.rb
+++ script/script.rb
@@ -1,15 +1,17 @@
 def substrings(word, array)
 
   array.reduce(Hash.new(0)) do |result, substring|
-    if word.include?(substring)
-      result[substring] += 1
+    if word.downcase.include?(substring)
+      result[substring] = word.downcase.scan(substring).length
     end
     result
   end
 
 end
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+# dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
  
 
-p substrings("below", dictionary)
+# p substrings("below", dictionary)
+
+# p substrings("Howdy partner, sit down! How's it going?", dictionary)
